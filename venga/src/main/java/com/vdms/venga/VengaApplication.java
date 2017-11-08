92322eb6acd4fc4d0a3f156484f6863676a41f46
Mayank Verma <mayank.verma@nviz.com>
2017-11-08 16:53:01 +0530

    basic code

---
+++ venga/src/main/java/com/vdms/venga/VengaApplication.java
@@ -0,0 +1,12 @@
+package com.vdms.venga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VengaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VengaApplication.class, args);
+	}
+}
